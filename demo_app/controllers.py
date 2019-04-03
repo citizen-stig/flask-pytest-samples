bda151da237b782a458f4b52ef8ef19065e15051
Nikolai Golub <nikolay.v.golub@gmail.com>
2019-04-03 16:36:55 +0200

    Add some write functionality

--- demo_app/controllers.py
+++ demo_app/controllers.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, redirect, url_for
+import string
+import random
+from flask import Blueprint, redirect, url_for, render_template
 
 from . import models
 
@@ -19,9 +21,41 @@ def list_categories():
 def create_category(name):
     if name:
         category = models.Category(name=name)
-        print('session: {}'.format(models.db.session))
+        print('"create_category" session: {}'.format(models.db.session))
         models.db.session.add(category)
         models.db.session.commit()
         return redirect(url_for('blog.list_categories'))
     return 'Please specify name'
 
+
+ALPHABET = string.ascii_uppercase + string.digits + ' '
+
+
+def rand_string(length, alphabet=ALPHABET):
+    return ''.join(random.choice(alphabet) for _ in range(length))
+
+
+@blog.route('/category/<string:category_name>/')
+def list_posts(category_name):
+    category = models.Category.query \
+        .filter(models.Category.name == category_name) \
+        .first_or_404()
+    return render_template('posts.html', category=category)
+
+
+@blog.route('/category/<string:category_name>/<int:posts_number>')
+def create_x_posts(category_name, posts_number):
+    category = models.Category.query \
+        .filter(models.Category.name == category_name)\
+        .first_or_404()
+
+    for x in range(posts_number):
+        post = models.Post(
+            title=rand_string(20),
+            body=rand_string(180, alphabet=ALPHABET + '\n'),
+            category=category,
+        )
+        models.db.session.add(post)
+        models.db.session.commit()
+
+    return redirect(url_for('blog.list_posts', category_name=category.name))
